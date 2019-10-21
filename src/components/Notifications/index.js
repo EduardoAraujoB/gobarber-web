5224bec7aab95bac552a69864efededfc89c4a4c
unknown <ea0123450@gmail.com>
2019-10-21 14:52:58 -0300

    profile photo

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -63,7 +63,7 @@ export default function Notifications() {
       <NotificationList visible={visible}>
         <Scroll>
           {notifications.map(notification => (
-            <Notification unread={!notification.read}>
+            <Notification key={notification._id} unread={!notification.read}>
               <p>{notification.content}</p>
               <time>{notification.timeDistance}</time>
               {!notification.read && (
